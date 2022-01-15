75daf4a299e59ab8cd68bef221bc0af5b9bfb493
kotarott <kg221kg@gmail.com>
2022-01-15 18:49:45 +0900

    update

--- QuestionTime/settings.py
+++ QuestionTime/settings.py
@@ -143,7 +143,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 
 REST_FRAMEWORK = {
--- QuestionTime/urls.py
+++ QuestionTime/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django_registration.backends.one_step.views import RegistrationView
 
+from core.views import IndexTemplateView
 from users.forms import CustomUserForm
 
 urlpatterns = [
@@ -34,4 +35,5 @@ urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
 
     path('api/v1/', include('questions.api.urls')),
+    re_path(r'^.*$', IndexTemplateView.as_view(), name='spa-entry-point'),
 ]
